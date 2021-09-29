2bf47a333dd1771f902c8049d2890de55a928d4c
Arynn Boniface <arynn.boniface@gmail.com>
2021-09-29 15:20:11 -0400

    dinking with redux

--- client/src/redux/actions/card.js
+++ client/src/redux/actions/card.js
@@ -1,13 +1,8 @@
-import { BASE_URL, GET_CARDS } from './index';
+import { GET_CARDS } from './index';
 // import axios from 'axios';
 
+const baseUrl = 'http://localhost:3000';
+
 export const getCards = () => dispatch => {
-  fetch(`${BASE_URL}/cards`)
-  .then(resp => {
-    if (resp.ok) {
-      return resp.json().then(cards => dispatch({ type: GET_CARDS, payload: cards.data }))
-    } else {
-      return Promise.reject();
-    };
-  });
+
 };
