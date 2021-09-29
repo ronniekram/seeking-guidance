1c0af9e34565bf432470f65ae7eded4199eff580
Arynn Boniface <arynn.boniface@gmail.com>
2021-09-29 10:58:23 -0400

    action index variables

---
+++ client/src/redux/actions/index.js
@@ -0,0 +1,4 @@
+export const BASE_URL = 'http://localhost:3000';
+export const GET_CARDS = 'GET_CARDS';
+export const GET_DRAWS = 'GET_DRAWS';
+export const POST_DRAW = 'POST_DRAW';
---
+++ client/src/redux/reducers/card.js
@@ -0,0 +1,5 @@
+const cardReducer = () => {
+
+};
+
+export default cardReducer;
