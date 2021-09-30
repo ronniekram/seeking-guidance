0d2ec74efeefc4745014241352e999e145a63244
Arynn Boniface <arynn.boniface@gmail.com>
2021-09-29 20:38:58 -0400

    redux pulling draw objs correctly - cant post card ids

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -2,12 +2,14 @@ import React, { useEffect } from 'react';
 import { useDispatch } from 'react-redux';
 import DrawForm from './draws/DrawForm';
 import { getCards } from '../redux/actions/card';
+import { getDraws } from '../redux/actions/draw';
 
 const App = () => {
   const dispatch = useDispatch();
 
   useEffect(() => {
     dispatch(getCards());
+    dispatch(getDraws())
   });
   
   return (
--- server/app/serializers/draw_serializer.rb
+++ server/app/serializers/draw_serializer.rb
@@ -1,4 +1,8 @@
 class DrawSerializer < ActiveModel::Serializer
-  attributes :id, :question, :card_ids
+  attributes :id, :question, :created_date
   has_many :cards
+
+  def created_date
+    object.created_at.strftime("asked %m/%d/%Y at %l:%M%P") 
+  end
 end
