3599322a4570747ee06ee875ee62f0f1eec743aa
Arynn Boniface <arynn.boniface@gmail.com>
2021-10-04 13:51:10 -0400

    globalizing link styling

--- client/src/components/draws/Draws.js
+++ client/src/components/draws/Draws.js
@@ -1,11 +1,26 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
+import { NavLink } from 'react-router-dom';
 import styles from '../../assets/styles/draws/draws.module.scss';
 
 const Draws = () => {
+  const {draws} = useSelector(state => state.draws);
+  
+  const renderDrawLinks = () =>
+    draws.map((draw) => (
+      <li key={draw.id}>
+        <NavLink to={`/draws/${draw.id}`}>
+          {draw.question}
+        </NavLink>
+      </li>
+    ));
+
   return (
-    <div>
-      List of past draws
-    </div>
+    <article className={styles.container}>
+      <div>
+        {renderDrawLinks()}
+      </div>
+    </article>
   );
 }
 
