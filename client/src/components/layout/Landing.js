223ed88d0cc79682070fbb5ea8b17c3b15636172
Arynn Boniface <arynn.boniface@gmail.com>
2021-10-04 12:41:55 -0400

    adding image on landing page

--- client/src/components/layout/Landing.js
+++ client/src/components/layout/Landing.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
+import starHand from '../../assets/svg/starHand.svg';
 import styles from '../../assets/styles/layout/landing.module.scss';
 
 const Landing = () => {
   return (
     <article className={styles.container}>
+
+      <img src={starHand} alt="Hand holding stars..." />
+
       <p>
         Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
       </p>
@@ -12,6 +16,7 @@ const Landing = () => {
       <p className={styles.links}>
         <NavLink to="/draws/new">Illuminate your path</NavLink> or <NavLink to="/draws">see paths from the past...</NavLink>
       </p>
+
     </article>
   );
 }
