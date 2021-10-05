2e0367d4d947e5b19cb06553a32d551272dc025b
Arynn Boniface <arynn.boniface@gmail.com>
2021-10-05 16:35:41 -0400

    adding page for single card draw

--- client/src/components/cards/Single.js
+++ client/src/components/cards/Single.js
@@ -1,10 +1,35 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
+import Card from './Card';
+import styles from '../../assets/styles/cards/single.module.scss';
 
 const Single = () => {
+  const { cards } = useSelector(state => state.cards);
+  const randomId = Math.floor(Math.random() * 78);
+  const card = cards[randomId];
+
+  const renderCard = () => {
+    if (card) {
+      return (
+        <article className={styles.container}>
+            <section className={styles.text}>
+              <Card card={card} />
+            </section>
+      
+            <section>
+              <img src={card.image} alt={card.name} className={styles.card} />
+            </section>
+        </article>
+      );
+    } else {
+      return null;
+    }
+  };
+
   return (
-    <div>
-      ONE CARD DRAW
-    </div>
+    <>
+      {renderCard()}
+    </>
   );
 };
 
--- client/src/components/draws/DrawForm.js
+++ client/src/components/draws/DrawForm.js
@@ -23,7 +23,7 @@ const DrawForm = () => {
     <div className={styles.container}>
       
       <div>
-
+        Illuminate your path...
       </div>
 
       <form onSubmit={handleSubmit}>
